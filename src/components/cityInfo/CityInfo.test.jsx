e8fc2ff60fd3e0d8001bdb7560c33ebb030ddb4a
Miguel Cabalero <miguelcaballeros.mc@ghotmail.com>
2021-12-27 11:43:42 -0500

    testing

---
+++ src/components/cityInfo/CityInfo.test.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react'
+import { render } from '@testing-library/react'
+import CityInfo from './CityInfo' // SUT: subject under testing (objeto del testeo)
+
+test("CityInfo render",async () =>{
+ // AAA 
+// Arrange-> todo lo que necesita el test 
+// Act -> hacer el test 
+const city = "Cartagena"
+const country = "Colombia"
+    const { findAllByRole } = render(<CityInfo city={city} country={country}></CityInfo>)
+// Assert -> revisar que el test funcionó
+    // buscar todos los componentes que sean cabecera (h1,h2,..)
+    const cityAndCountryComponent = await findAllByRole("heading")//aunque muestra error funciona bien
+    // ¿Cuando es correcto el test?
+    // Definicion
+    /*
+        Cuando el primere elemento (Heading) se encyuentre en "Cartagena" y cunado el segundo se encuente en "Colombia"
+    */
+    expect(cityAndCountryComponent[0]).toHaveTextContent(city)
+    expect(cityAndCountryComponent[1]).toHaveTextContent(country)
+})
